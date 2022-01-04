fdb4c230fa05e6faa1e1568de9fdb3f7718d93a6
arseniy <arseniy.karmanov@mail.ru>
2022-01-05 01:59:16 +0900

    #HW3-1 is done

--- src/main/java/ru/gb/homework/App.java
+++ src/main/java/ru/gb/homework/App.java
@@ -1,8 +1,36 @@
 package ru.gb.homework;
 
+import java.util.*;
+
 public class App {
 
     public static void main(String[] args) {
+        String[] strings = {
+                "b",
+                "C",
+                "A",
+                "b",
+                "V",
+                "C",
+                "A",
+                "C"
+        };
 
+        List<String> list = Arrays.asList(strings);
+
+        System.out.println(list);
+
+        Set<String> set = new HashSet<>(list);
+
+        System.out.println(set);
+
+        for (String s1 : set) {
+            int count = 0;
+            for (String s2 : list) {
+                if (s1.equals(s2))
+                    count++;
+            }
+            System.out.printf("\"%s\" встречается раз: %d\n", s1, count);
+        }
     }
 }
